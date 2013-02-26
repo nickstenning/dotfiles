0efa1f93bf3f5b14779f749673c5b299b1ce981e
Nick Stenning <nick.stenning@digital.cabinet-office.gov.uk>
2013-02-26 17:20:55 +0000

    Bundle install alias

--- zsh/lib/commands.zsh
+++ zsh/lib/commands.zsh
@@ -81,6 +81,7 @@ alias lalh="lal -h"
 alias aka-curl='curl -H "Pragma: akamai-x-cache-on, akamai-x-cache-remote-on, akamai-x-check-cacheable, akamai-x-get-cache-key, akamai-x-get-extracted-values, akamai-x-get-nonces, akamai-x-get-ssl-client-session-id, akamai-x-get-true-cache-key, akamai-x-serial-no"'
 alias aka-dcurl='aka-curl -s -D- -o/dev/null'
 alias be="bundle exec"
+alias bi="bundle install --path=.gems"
 alias dcurl='curl -s -D- -o/dev/null'
 alias d="dirs -v"
 alias ducks="du -ks -- * | sort -nr | cut -f2 | tr '\n' '\0' | du -shc --files0-from=-"
