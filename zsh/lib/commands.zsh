96b7d42bc956dc674ca770a3c6794557152936dd
Nick Stenning <nick@whiteink.com>
2013-03-13 08:46:28 +0000

    Inherit OS X keychain and ignore missing keys

--- zsh/lib/commands.zsh
+++ zsh/lib/commands.zsh
@@ -25,7 +25,7 @@ histstat() {
 }
 
 ks () {
-  eval $(keychain -q --eval --timeout 1440 ${=KEYCHAIN_DEFAULT_KEYS})
+  eval $(keychain -q --inherit any --ignore-missing --eval --timeout 1440 ${=KEYCHAIN_DEFAULT_KEYS})
 }
 
 mess () {
