d86ee5ee37fb74093d5f2323e1bb2c1ba7032ff7
Nick Stenning <nick.stenning@digital.cabinet-office.gov.uk>
2013-03-27 16:16:42 +0000

    Don't check history expansions

--- zsh/lib/history.zsh
+++ zsh/lib/history.zsh
@@ -8,5 +8,5 @@ setopt extended_history
 setopt hist_expire_dups_first
 setopt hist_ignore_dups # ignore duplication command history list
 setopt hist_ignore_space
-setopt hist_verify
+setopt no_hist_verify
 setopt inc_append_history
