2de0ac9547f01e29cf8a4bb806322e8942066017
Nick Stenning <nick@whiteink.com>
2025-02-15 13:52:44 +0100

    Switch from fzf to atuin for history

--- zsh/lib/keys.zsh
+++ zsh/lib/keys.zsh
@@ -23,18 +23,6 @@ bindkey "\e\e[D" backward-word # alt-leftarrow
 bindkey -M viins '^A' beginning-of-line
 bindkey -M viins '^E' end-of-line
 
-# history search
-if command -v fzf >/dev/null 2>&1; then
-  dot "$HOMEBREW_PREFIX/opt/fzf/shell/key-bindings.zsh"
-else
-  echo "warning: fzf not installed"
-  history-incremental-search-{back,for}ward() zle .$WIDGET -- $BUFFER
-  zle -N history-incremental-search-backward
-  zle -N history-incremental-search-forward
-  bindkey '^R' history-incremental-search-backward
-fi
-
-
 if [[ -e ~/.zkbd/"$TERM-$VENDOR-$OSTYPE" ]]; then
   source ~/.zkbd/"$TERM-$VENDOR-$OSTYPE"
   bindkey "${key[Home]}" beginning-of-line
